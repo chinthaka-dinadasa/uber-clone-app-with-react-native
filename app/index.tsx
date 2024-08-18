a18cc72320f0d95b9e0bbaae375afacb405518b0
Chinthaka Dinadasa <sendtoclabz@gmail.com>
2024-08-18 17:02:12 +0530

    code refactor and added latest assets

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <Text>Homesssssss</Text>
+    </SafeAreaView>
+  );
+};
+export default Home;
